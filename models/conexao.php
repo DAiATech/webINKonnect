62d181eb5b43b4548ec24a363f5892915a5771fa
Arthur Ribeiro Dias Fudali <arthurfudali5@gmail.com>
2023-09-28 21:33:36 -0300

    variavel pdo

--- models/conexao.php
+++ models/conexao.php
@@ -3,6 +3,7 @@ $servidor = "127.0.0.1";
 $usuario = "root";
 $senha = "";
 $banco = "inkonnect";
+$pdo = new PDO('mysql:host=127.0.0.1;dbname=appinkonnect','root','');
 $conexao =  mysqli_connect($servidor, $usuario, $senha);
             mysqli_select_db($conexao, $banco);
             mysqli_set_charset($conexao, "UTF8");
