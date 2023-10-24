95517eb3b71f88434994dbf12b4971e8986b68e5
Arthur Fudali <arthur.fudali@etec.sp.gov.br>
2023-10-24 08:32:48 -0300

    exibição dos posts

--- cms/models/feed.php
+++ cms/models/feed.php
@@ -6,19 +6,38 @@
     $getInfoPosts = getInfoPosts();
     $dadosprofile = mysqli_query($conexao, $getInfoPosts);
 
-    while ($resultprofile = mysqli_fetch_array($dadosprofile)) {?>
+    while ($resultprofile = mysqli_fetch_array($dadosprofile)) { ?>
 
-        
-        <img src="../../views/tatuador/imgsprofile/<?php echo $resultprofile['profileImgRandomName']; ?>" alt="imagem de perfil">         
-        <img src="../views/tatuador/imgsposts/<?php echo $resultprofile['postagemImgRandomName']; ?>" alt="imagem da postagem">         
+        <div class="profile">
+            <div class="divImgProfile p-3">
+                <img class="imgProfile fluid p-3"
+                    src="../../views/tatuador/imgsprofile/<?php echo $resultprofile['profileImgRandomName']; ?>"
+                    alt="imagem de perfil">
+            </div>
+            <div class="m-3">
+                <p>
+                    <?php echo $resultprofile['nome']; ?>
+                </p>
+            </div>
+        </div>
 
-        <?php echo $resultprofile['descricao']; ?>
-        <?php echo $resultprofile['estilo']; ?>
+        <div class="img-container">
+            <img src="../views/tatuador/imgsposts/<?php echo $resultprofile['postagemImgRandomName']; ?>"
+                alt="imagem da postagem">
+        </div>
+        <div class="info-posts">
 
-    <?php
+            <?php echo $resultprofile['descricao']; ?>
+            <?php echo $resultprofile['estilo']; ?>
+
+        </div>
+
+
+
+        <?php
     }
     ?>
 
-    
+
 
 </div>
