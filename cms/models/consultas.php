1f283ee10c61b5fdcb74792fabf2960f009def4f
Arthur Fudali <arthur.fudali@etec.sp.gov.br>
2023-10-24 11:03:07 -0300

    inicio da exibição do perfil

--- cms/models/consultas.php
+++ cms/models/consultas.php
@@ -1,19 +1,30 @@
 <?php
 session_start();
 
-if (isset($_SESSION["nivel"]) && $_SESSION["nivel"] == 'cliente') {
+/* if (isset($_SESSION["nivel"]) && $_SESSION["nivel"] == 'cliente') {
     $tabela = "usuarios";
-    $id = $_SESSION["id"];
 } elseif (isset($_SESSION["nivel"]) && $_SESSION["nivel"] == 'tatuador') {
     $tabela = "tatuador";
-    $id = $_SESSION["id"];
-}
-function getInfoProfile()
+} */
+$id = $_SESSION["id"];
+function getInfoProfileUser()
 {
-    global $tabela;
+    /*  global $tabela; */
     global $id;
-    return "SELECT * from $tabela where id = '$id'";
+    return "SELECT * from usuarios where id = '$id'";
 }
+function getInfoProfileTatuador()
+{
+    global $id;
+    return "SELECT postagenstatuadores.*, tatuador.*,postagensimg.imgRandomName AS postagemImgRandomName, 
+    profileimg.imgRandomName AS profileImgRandomName  
+    FROM postagenstatuadores 
+    INNER JOIN postagensimg ON imgPostId = postagensimg.id
+    INNER JOIN tatuador ON tatuadorId = tatuador.id
+    INNER JOIN profileimg ON tatuador.profileImgId = profileimg.id
+    WHERE postagenstatuadores.tatuadorId = $id";
+}
+
 function getPostsTatuador()
 {
     global $id;
--- cms/models/perfil.php
+++ cms/models/perfil.php
@@ -3,12 +3,38 @@
     <?php
     include("consultas.php");
     include("../../models/conexao.php");
-    $getInfoProfile = getInfoProfile();
-    $dadosprofile = mysqli_query($conexao, $getInfoProfile);
-    echo "id = $id" ;
-    while ($resultprofile = mysqli_fetch_array($dadosprofile)) { ?>
 
-        <?php echo $resultprofile['descricao']; ?>
+    if (isset($_SESSION["nivel"]) && $_SESSION["nivel"] == 'cliente') {
+        $getInfoProfile = getInfoProfileUser();
+        $nivel = 'cliente';
+
+    } elseif (isset($_SESSION["nivel"]) && $_SESSION["nivel"] == 'tatuador') {
+        $getInfoProfile = getInfoProfileTatuador();
+        $nivel = 'tatuador';
+    } else {
+        echo 'erro, nenhuma sessão disponível';
+    }
+    $dadosprofile = mysqli_query($conexao, $getInfoProfile);
+    $resultprofile = mysqli_fetch_array($dadosprofile);
+    if ($nivel == 'cliente') { ?>
+
+
+    <?php } else if ($nivel == 'tatuador') { ?>
+
+        <?php echo $resultprofile['nome']; ?>
+            <img src="../../views/tatudor/imgsprofile/<?php echo $resultprofile['profileImgRandomName']; ?>" alt="">
+
+     <?php } else {
+        echo "nivel invalido";
+    } ?> alt="imagem de perfil do tatuador">
+
+
+
+    <?php
+    while ($resultprofile) { ?>
+
+
+        <?php echo $resultprofile['postagemImgRandomName']; ?>
 
 
     <?php } ?>
